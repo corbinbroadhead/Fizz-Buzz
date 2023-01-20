95b3a02c07216bc6de9d6260580a126885782683
Corbin Broadhead <poujmail@gmail.com>
2023-01-19 19:44:21 -0700

    Fin.

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,15 @@
 // Write your solution below this line:
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Fizzbuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+let howDidTheGameGo = "Great! Scorps won 41 to nothing.";
